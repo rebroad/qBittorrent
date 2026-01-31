306cc953ba13eaa86917f23d4f035b0ebe4ce8dc
R E Broadley <rebroad@users.noreply.github.com>
2026-01-30 20:30:10 -0500

    Add additional announce IP support for dual-homed/tunnel setups
    - Session: additionalAnnounceIP config, sendAdditionalAnnounceToTrackers()
      with periodic HTTP announces using ip= parameter for second IP
    - API/Web UI: additional_announce_ip preference
    - GUI: Advanced settings row for Additional announce IP
    Allows advertising a second IP to trackers (e.g. relay + local public).

--- src/base/bittorrent/session.h
+++ src/base/bittorrent/session.h
@@ -393,6 +393,8 @@ namespace BitTorrent
         virtual void setIncludeOverheadInLimits(bool include) = 0;
         virtual QString announceIP() const = 0;
         virtual void setAnnounceIP(const QString &ip) = 0;
+        virtual QString additionalAnnounceIP() const = 0;
+        virtual void setAdditionalAnnounceIP(const QString &ip) = 0;
         virtual int announcePort() const = 0;
         virtual void setAnnouncePort(int port) = 0;
         virtual int maxConcurrentHTTPAnnounces() const = 0;
